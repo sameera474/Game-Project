0bd4fc280644b05b24b5e2ca89a32fc19e98fc1b
Sameera Chaturanga Wagaarachchige <118694605+sameera474@users.noreply.github.com>
2024-03-05 11:13:17 +0200

    add the image tags

--- script.js
+++ script.js
@@ -24,8 +24,28 @@ const cardArray = [
     img: "images/pizza.png",
   },
   {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
     name: "cheeseburger",
     img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
   },
 ];
 
@@ -38,4 +58,12 @@ shufflearray(cardArray);
 const result = document.querySelector("#result");
 const board = document.querySelector("#board");
 
-console.log(board);
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    console.log(card, i);
+  }
+}
+
+createBoard();
